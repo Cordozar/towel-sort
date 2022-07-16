546460361174da668ff333b6d5dc70ccb325e69c
Cordozar <cant09y@gmail.com>
2022-07-16 18:06:25 +0300

    init: decided tower sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+  if (!matrix) {
+    return []
+  }  
+  for (let i = 1; i < matrix.length; i += 2) {
+      matrix[i] = matrix[i].reverse();
+    }
+    let res = [];
+    for (let i = 0; i < matrix.length; i++) {
+        res = res.concat(matrix[i]);
+    }
+    return res
+
+};
